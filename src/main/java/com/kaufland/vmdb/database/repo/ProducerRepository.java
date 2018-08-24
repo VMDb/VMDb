a63697e1c052fe4238112131e350247950456da2
Viad <vladi_441@abv.bg>
2018-08-24 09:03:01 +0300

    Created Producer repo

---
+++ src/main/java/com/kaufland/vmdb/database/repo/ProducerRepository.java
@@ -0,0 +1,9 @@
+package com.kaufland.vmdb.database.repo;
+
+import com.kaufland.vmdb.domain.Producer;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProducerRepository extends HumanRepository<Producer> {
+
+}
